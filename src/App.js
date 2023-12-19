04d0ca678c2ad861da96e712356a22da5d139370
Steven88ly <steven88ly@gmail.com>
2023-12-19 15:40:07 -0500

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
+import Users from './user/pages/Users.js';
+import NewPlace from './places/pages/NewPlace.js';
 
-function App() {
-  return <h1>Let's start!</h1>;
-  
+const App = () => {
+  return <Router>
+    <Switch>
+      <Route path='/' exact component={Users}/>  
+      <Route path='/places/new' exact component={NewPlace}/>
+      <Redirect to='/'/>
+    </Switch>
+  </Router>
 }
 
 export default App;
