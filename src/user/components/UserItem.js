702e67a4e18db2b9ca900595070cdf488ca59b47
Steven88ly <steven88ly@gmail.com>
2023-12-19 16:00:07 -0500

    created UsersList component and dummy data

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./css/UserItem.css"
+
+const UserItem = () => {
+    return
+}
+
+export default UserItem
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return <h2>User works!</h2>
-       
-}
-export default Users
+  const USERS = [
+    {
+      id: "u1",
+      name: "kathleen",
+      image:
+        "https://unsplash.com/photos/woman-in-black-sleeveless-top-standing-beside-fence-NVi2yab124g",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
+export default Users;
