2a50593032cfd8dacece1c7ae26d6b7f54ff3c5b
Steven88ly <steven88ly@gmail.com>
2023-12-23 20:59:59 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
-import Users from './user/pages/Users.js';
-import NewPlace from './places/pages/NewPlace.js';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./user/pages/Users.js";
+import NewPlace from "./places/pages/NewPlace.js";
+import MainNavigation from "./places/shared/components/Navigation/MainNavigation.js";
 
 const App = () => {
-  return <Router>
-    <Switch>
-      <Route path='/' exact component={Users}/>  
-      <Route path='/places/new' exact component={NewPlace}/>
-      <Redirect to='/'/>
-    </Switch>
-  </Router>
-}
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact component={Users} />
+          <Route path="/places/new" exact component={NewPlace} />
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
