d93f07d4a69fa5f689ab5a64f497b90f82afcf55
Steven88ly <steven88ly@gmail.com>
2024-01-02 13:16:34 -0500

    kathy branch init

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,44 +1,40 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import MainHeader from "./Mainheader";
-import NavLinks from "./NavLinks";
-import SideDrawer from "./SideDrawer";
-import Backdrop from "../UIElements/Backdrop/Backdrop";
+import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
+import './MainNavigation.css';
 
-import "./MainNavigation.css";
-
-const MainNavigation = (props) => {
+const MainNavigation = props => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
   const openDrawerHandler = () => {
     setDrawerIsOpen(true);
-  }
+  };
 
   const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
-  }
+  };
 
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/>}
-
-    
-      <SideDrawer onClick={closeDrawerHandler} show={drawerIsOpen}>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
-     
 
       <MainHeader>
         <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
-          <span></span>
-          <span></span>
-          <span></span>
+          <span />
+          <span />
+          <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">Your Places</Link>
+          <Link to="/">YourPlaces</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
