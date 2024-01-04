8425cb5788f2704f82cf6b38b16a8cdba8f15f63
Steven88ly <steven88ly@gmail.com>
2024-01-03 20:35:53 -0500

    adding new place form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/components/util/validators";
+
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+  return (
+    <form className="place-form">
+      <Input
+        label="Title"
+        element="input"
+        type="text"
+        validators={[VALIDATOR_REQUIRE]}
+        errorText="Please enter a valid title"
+      ></Input>
+    </form>
+  );
 };
 
 export default NewPlace;
